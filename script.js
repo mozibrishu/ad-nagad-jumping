966b970b8de27d5e1a6f1468fcf2841683a1c624
Mozib <mozib.diu@gmail.com>
2022-10-21 22:38:49 +0600

    line by line working

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-
+gsap.fromTo('.pp_slide_1 .pp_element_3', { scaleX: 1, scaleY: 1 }, { scaleX: 1.2, scaleY: 1.1, duration: 1.2, repeat: -1, yoyo: true });
+document.querySelector(".pp_slide_1 .pp_element_3").addEventListener('click', () => {
+  console.log('working');
+  gsap.timeline()
+  .to('.pp_slide_1',{opacity:0,duration:.5})
+  .fromTo('.pp_slide_2',{opacity:.5,display:'none'},{opacity:1,display:'block',duration:.8},">-.3");
+  setTimeout(() => {
+    setCheckInterval();
+gameAnimation();
+  }, 1000);
+});
 
 function jump() {
   document.querySelector('.pp_slide_2').removeEventListener('click', jump);
@@ -12,8 +22,7 @@ function jump() {
 
 document.querySelector('.pp_slide_2').addEventListener('click', jump);
 
-setCheckInterval();
-gameAnimation();
+
 // clearCheckInterval();
 function clearCheckInterval() {
   clearInterval(obstacleCheckInterval);
@@ -36,38 +45,38 @@ function setCheckInterval(params) {
   }, 100);
 }
 
-function gameAnimation(){
-  t1 = gsap.timeline({onComplete: clearCheckInterval, delay: 2.5 })
-  .fromTo('.pp_slide_2 .pp_element_1', { x: 0 }, { x: -1920, duration: 20, ease: Linear.easeNone })
-  .to('.pp_slide_2 .pp_element_2', { x: 220, duration: 2.5, ease: Linear.easeNone }, 20)
-  .fromTo('.pp_slide_2 .pp_element_4', { scaleX: 1, scaleY: 1, opacity: 1 }, { scaleX: 1.1, scaleY: 1.1, opacity: .9, duration: .5, repeat: 2, yoyo: true }, -1.5)
-  .fromTo('.pp_slide_2 .pp_element_4', { opacity: 1 }, { opacity: 0, display: "none", duration: .3 }, ">")
-  // coin animation
-  .fromTo('.pp_slide_2 .pp_element_3', { x: 0 }, { x: -100, duration: 1, ease: Linear.easeNone }, 1.5)
-  .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: -20, duration: .5, ease: Linear.easeNone }, '>')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: -100, duration: 2.6, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: -20, duration: .5, ease: Linear.easeNone }, '>')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: -100, duration: 2.6, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: -20, duration: .5, ease: Linear.easeNone }, '>')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: -100, duration: 2.6, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: -20, duration: .5, ease: Linear.easeNone }, '>')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: -100, duration: 2.6, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: -20, duration: .5, ease: Linear.easeNone }, '>')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: 0, x: 200, duration: .1, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: 200, duration: .1, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: -100, duration: 3, ease: Linear.easeNone }, '>.1')
-  .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: -20, duration: .5, ease: Linear.easeNone }, '>')
-  // obstacle animation
-  .fromTo('.pp_slide_2 .pp_element_5', { left: 130 }, { left: 30, duration: 1, ease: Linear.easeNone }, 1.5)
-  .fromTo('.pp_slide_2 .pp_element_5', { left: 350 }, { left: 30, duration: 3.2, ease: Linear.easeNone }, 4)
-  .fromTo('.pp_slide_2 .pp_element_5', { left: 290 }, { left: 30, duration: 2.9, ease: Linear.easeNone }, 12)
-  .fromTo('.pp_slide_2 .pp_element_5', { left: 265 }, { left: 30, duration: 2.65, ease: Linear.easeNone }, 16)
+function gameAnimation() {
+  t1 = gsap.timeline({ onComplete: clearCheckInterval, delay: 2.5 })
+    .fromTo('.pp_slide_2 .pp_element_1', { x: 0 }, { x: -1920, duration: 20, ease: Linear.easeNone })
+    .to('.pp_slide_2 .pp_element_2', { x: 220, duration: 2.5, ease: Linear.easeNone }, 20)
+    .fromTo('.pp_slide_2 .pp_element_4', { scaleX: 1, scaleY: 1, opacity: 1 }, { scaleX: 1.1, scaleY: 1.1, opacity: .9, duration: .5, repeat: 2, yoyo: true }, -1.5)
+    .fromTo('.pp_slide_2 .pp_element_4', { opacity: 1 }, { opacity: 0, display: "none", duration: .3 }, ">")
+    // coin animation
+    .fromTo('.pp_slide_2 .pp_element_3', { x: 0 }, { x: -100, duration: 1, ease: Linear.easeNone }, 1.5)
+    .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: -20, duration: .5, ease: Linear.easeNone }, '>')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: -100, duration: 2.6, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: -20, duration: .5, ease: Linear.easeNone }, '>')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: -100, duration: 2.6, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: -20, duration: .5, ease: Linear.easeNone }, '>')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: -100, duration: 2.6, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: -20, duration: .5, ease: Linear.easeNone }, '>')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: 160, duration: .1, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: -100, duration: 2.6, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: -20, duration: .5, ease: Linear.easeNone }, '>')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: 0, x: 200, duration: .1, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: 200, duration: .1, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 1, y: 0, x: -100, duration: 3, ease: Linear.easeNone }, '>.1')
+    .to('.pp_slide_2 .pp_element_3', { opacity: 0, y: -20, duration: .5, ease: Linear.easeNone }, '>')
+    // obstacle animation
+    .fromTo('.pp_slide_2 .pp_element_5', { left: 130 }, { left: 30, duration: 1, ease: Linear.easeNone }, 1.5)
+    .fromTo('.pp_slide_2 .pp_element_5', { left: 350 }, { left: 30, duration: 3.2, ease: Linear.easeNone }, 4)
+    .fromTo('.pp_slide_2 .pp_element_5', { left: 290 }, { left: 30, duration: 2.9, ease: Linear.easeNone }, 12)
+    .fromTo('.pp_slide_2 .pp_element_5', { left: 265 }, { left: 30, duration: 2.65, ease: Linear.easeNone }, 16)
 }
