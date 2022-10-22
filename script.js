d5e8795316a2fe13f35db7f3fee11ed26b8b1795
Mozib <mozib.diu@gmail.com>
2022-10-22 19:02:51 +0600

    button added

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function setCheckInterval(params) {
 function gameAnimation() {
   t1 = gsap.timeline({ onComplete: clearCheckInterval, delay: 2.5 })
     .fromTo('.pp_slide_2 .pp_element_1', { x: 0 }, { x: -1920, duration: 20, ease: Linear.easeNone })
-    .to('.pp_slide_2 .pp_element_2', { x: 220, duration: 2.5, ease: Linear.easeNone }, 20)
+    .to('.pp_slide_2 .pp_element_2', { x: 220, duration: 2.2, ease: Linear.easeNone }, 20)
     .fromTo('.pp_slide_2 .pp_element_4', { scaleX: 1, scaleY: 1, opacity: 1 }, { scaleX: 1.1, scaleY: 1.1, duration: .5, repeat: 2, yoyo: true }, -1.5)
     .fromTo('.pp_slide_2 .pp_element_4', { opacity: 1 }, { opacity: 0, display: "none", duration: .3 }, ">")
     // coin animation
